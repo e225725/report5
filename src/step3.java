03693d538a593cda41da688d810e925c3f8489cc
applerm <e225725@ie.u-ryukyu.ac.jp>
2023-02-24 00:26:16 +0900

    3rd commit

---
+++ src/step3.java
@@ -0,0 +1,12 @@
+public class step3 {
+    
+
+    public static void main(String[] args) {
+
+        String test="壱百満";
+        int ConvertTest=Integer.parseInt(test);
+
+
+        System.out.println(ConvertTest);
+    }
+}
